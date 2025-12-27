28cffaf0ec8f0d38c67b965b54b8b7c11f87377f
one <one@debmin86-x64.rlab>
2025-12-27 19:50:41 +0300

    Added neo-tree, bufferline; updated keybindings

--- init.lua
+++ init.lua
@@ -160,21 +160,33 @@ require('lazy').setup({
     build = ":TSUpdate",
   },
 
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
+  -- gk
+  {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    opts = {
+      options = {
+        numbers = "buffer_id",
+      },
+    },
+  },
 
-  -- NOTE: The import below automatically adds your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  --
-  --    An additional note is that if you only copied in the `init.lua`, you can just comment this line
-  --    to get rid of the warning telling you that there are not plugins in `lua/custom/plugins/`.
-  { import = 'plugins' },
+  -- gk
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  },
+
+
+
+
 }, {})
 
 -- [[ Setting options ]]
@@ -186,7 +198,7 @@ vim.o.hlsearch = true
 -- Make line numbers default
 vim.wo.number = true
 
-vim.o.mouse = ''
+-- vim.o.mouse = ''
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
@@ -483,3 +495,35 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+
+
+
+-- [[ gk Configure neo-tree]]
+
+vim.keymap.set("n", "<leader>e", "<Cmd>Neotree toggle<CR>")
+
+
+-- [[ gk Configure bufferline]]
+
+vim.keymap.set("n", "[b", ":bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "]b", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set('n', '<leader>1', '<cmd>buffer 1<CR>', { desc = "Go to buffer 1" })
+vim.keymap.set('n', '<leader>2', '<cmd>buffer 2<CR>', { desc = "Go to buffer 2" })
+vim.keymap.set('n', '<leader>3', '<cmd>buffer 3<CR>', { desc = "Go to buffer 3" })
+vim.keymap.set('n', '<leader>4', '<cmd>buffer 4<CR>', { desc = "Go to buffer 4" })
+vim.keymap.set('n', '<leader>5', '<cmd>buffer 5<CR>', { desc = "Go to buffer 5" })
+vim.keymap.set('n', '<leader>6', '<cmd>buffer 6<CR>', { desc = "Go to buffer 6" })
+vim.keymap.set('n', '<leader>7', '<cmd>buffer 7<CR>', { desc = "Go to buffer 7" })
+vim.keymap.set('n', '<leader>8', '<cmd>buffer 8<CR>', { desc = "Go to buffer 8" })
+vim.keymap.set('n', '<leader>9', '<cmd>buffer 9<CR>', { desc = "Go to buffer 9" })
+
+ --[[ commented because defined at the time of plugin loading
+require('bufferline').setup {
+  options = {
+    numbers = "buffer_id",
+  },
+}
+]]
+
+
